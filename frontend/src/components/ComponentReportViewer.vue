7dd1952a9be3560189c6a9a40f34fe6e43ae631e
guy <mail.guy.yaffe@gmail.com>
2020-04-15 14:39:33 +0300

    Initial table for reports (rely on local data, not from Server)

--- frontend/src/components/ComponentReportViewer.vue
+++ frontend/src/components/ComponentReportViewer.vue
@@ -3,8 +3,33 @@
 
     <div style="margin-top: 10%">
       {{ route_path }}
-    </div>
 
+    </div>
+    <template>
+        <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :loading="loading"
+        fixed
+        style="overflow-y: auto; overflow-x: auto"
+        no-data-text="No data to display"
+        :sort-by="['calories', 'fat']"
+        :sort-desc="[false, true]"
+        multi-sort
+        class="elevation-1"
+      >
+          <template slot="items" slot-scope="props">
+            <tr>
+              <td>{{ props.item.name }}</td>
+              <td class="text-xs-right">{{ props.item.calories }}</td>
+              <td class="text-xs-right">{{ props.item.fat }}</td>
+              <td class="text-xs-right">{{ props.item.carbs }}</td>
+              <td class="text-xs-right">{{ props.item.protein }}</td>
+              <td class="text-xs-right">{{ props.item.iron }}</td>
+            </tr>
+          </template>
+        </v-data-table>
+    </template>
   </v-container>
 </template>
 
@@ -30,7 +55,102 @@
         hideRow: false,
         hideHeaderRow: false,
         rows: [],
-        drawer: false
+        drawer: false,
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name'
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' }
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 200,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%'
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 200,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%'
+          },
+          {
+            name: 'Eclair',
+            calories: 300,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%'
+          },
+          {
+            name: 'Cupcake',
+            calories: 300,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%'
+          },
+          {
+            name: 'Gingerbread',
+            calories: 400,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%'
+          },
+          {
+            name: 'Jelly bean',
+            calories: 400,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%'
+          },
+          {
+            name: 'Lollipop',
+            calories: 400,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%'
+          },
+          {
+            name: 'Honeycomb',
+            calories: 400,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%'
+          },
+          {
+            name: 'Donut',
+            calories: 500,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%'
+          },
+          {
+            name: 'KitKat',
+            calories: 500,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%'
+          }
+        ]
       }
     },
     methods: {
@@ -138,6 +258,11 @@
   </script>
 
 <style scoped>
+  table.v-table tbody td,
+    table.v-table tbody th {
+    height: 19px;
+  }
+
   .page-content {
     display: flex;
     flex-direction: column;

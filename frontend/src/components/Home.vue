f0f6e54e5db7c684afe2aae83ff487261b7ecb8c
idancw <idancw@gmail.com>
2020-04-18 18:13:26 +0300

    Clean redundant files & change project name

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -29,60 +29,60 @@
                 <h2 class="mb-3 text-xs-center">All in one tool</h2>
               </div>
             </v-flex>
-            <v-flex xs12>
-              <v-container grid-list-xl>
-                <v-layout row wrap align-center>
-                  <v-flex xs12 md4 style="margin-bottom: 80px;">
-                    <v-card class="elevation-0 transparent">
-                      <v-card-text class="text-xs-center">
-                        <v-icon x-large class="blue--text text--lighten-2">done</v-icon>
-                      </v-card-text>
-                      <v-card-title primary-title class="layout justify-center">
-                        <div class="headline text-xs-center">Dataset Uploader</div>
-                      </v-card-title>
-                      <v-card-text style="text-align: justify;">
-                        The Dataset Uploader allows the easy creation of a dataset while allowing the
-                        user to name the dataset, create the tags that will be used for it and
-                        upload the images that will be tagged with the purpose of creating a ground truth.
-                      </v-card-text>
-                    </v-card>
-                   </v-flex>
-                  <v-flex xs12 md4>
-                    <v-card class="elevation-0 transparent">
-                      <v-card-text class="text-xs-center">
-                        <v-icon x-large class="blue--text text--lighten-2">how_to_reg</v-icon>
-                      </v-card-text>
-                      <v-card-title primary-title class="layout justify-center">
-                        <div class="headline">Dataset Manager</div>
-                      </v-card-title>
-                      <v-card-text style="text-align: justify;">
-                        The Dataset Manager allows the user to load a dataset which consists of instances
-                        of images and a set of tags. The user can than choose for each instance the correct
-                        tags that best describe it. The Dataset Manager offers the user a convenient interface
-                        which allows an easy way to save the current state of the dataset and the ability to
-                        clear the already tagged instances from the Dataset Manager page or show it again.
-                      </v-card-text>
-                    </v-card>
-                  </v-flex>
-                  <v-flex xs12 md4 style="margin-bottom: 43px;">
-                    <v-card class="elevation-0 transparent">
-                      <v-card-text class="text-xs-center">
-                        <v-icon x-large class="blue--text text--lighten-2">perm_data_setting</v-icon>
-                      </v-card-text>
-                      <v-card-title primary-title class="layout justify-center">
-                        <div class="headline text-xs-center">Evaluator</div>
-                      </v-card-title>
-                      <v-card-text style="text-align: justify;">
-                        The Evaluator allows the user to compare between a ground truth set (tagged set of images)
-                        and the output of a classification algorithm. The Evaluator creates a confusion matrix
-                        comparing between the algorithm and the ground truth as well as a convenient display of
-                        the images along with the result of the comparison for each instance.
-                      </v-card-text>
-                    </v-card>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-flex>
+<!--            <v-flex xs12>-->
+<!--              <v-container grid-list-xl>-->
+<!--                <v-layout row wrap align-center>-->
+<!--                  <v-flex xs12 md4 style="margin-bottom: 80px;">-->
+<!--                    <v-card class="elevation-0 transparent">-->
+<!--                      <v-card-text class="text-xs-center">-->
+<!--                        <v-icon x-large class="blue&#45;&#45;text text&#45;&#45;lighten-2">done</v-icon>-->
+<!--                      </v-card-text>-->
+<!--                      <v-card-title primary-title class="layout justify-center">-->
+<!--                        <div class="headline text-xs-center">Dataset Uploader</div>-->
+<!--                      </v-card-title>-->
+<!--                      <v-card-text style="text-align: justify;">-->
+<!--                        The Dataset Uploader allows the easy creation of a dataset while allowing the-->
+<!--                        user to name the dataset, create the tags that will be used for it and-->
+<!--                        upload the images that will be tagged with the purpose of creating a ground truth.-->
+<!--                      </v-card-text>-->
+<!--                    </v-card>-->
+<!--                   </v-flex>-->
+<!--                  <v-flex xs12 md4>-->
+<!--                    <v-card class="elevation-0 transparent">-->
+<!--                      <v-card-text class="text-xs-center">-->
+<!--                        <v-icon x-large class="blue&#45;&#45;text text&#45;&#45;lighten-2">how_to_reg</v-icon>-->
+<!--                      </v-card-text>-->
+<!--                      <v-card-title primary-title class="layout justify-center">-->
+<!--                        <div class="headline">Dataset Manager</div>-->
+<!--                      </v-card-title>-->
+<!--                      <v-card-text style="text-align: justify;">-->
+<!--                        The Dataset Manager allows the user to load a dataset which consists of instances-->
+<!--                        of images and a set of tags. The user can than choose for each instance the correct-->
+<!--                        tags that best describe it. The Dataset Manager offers the user a convenient interface-->
+<!--                        which allows an easy way to save the current state of the dataset and the ability to-->
+<!--                        clear the already tagged instances from the Dataset Manager page or show it again.-->
+<!--                      </v-card-text>-->
+<!--                    </v-card>-->
+<!--                  </v-flex>-->
+<!--                  <v-flex xs12 md4 style="margin-bottom: 43px;">-->
+<!--                    <v-card class="elevation-0 transparent">-->
+<!--                      <v-card-text class="text-xs-center">-->
+<!--                        <v-icon x-large class="blue&#45;&#45;text text&#45;&#45;lighten-2">perm_data_setting</v-icon>-->
+<!--                      </v-card-text>-->
+<!--                      <v-card-title primary-title class="layout justify-center">-->
+<!--                        <div class="headline text-xs-center">Evaluator</div>-->
+<!--                      </v-card-title>-->
+<!--                      <v-card-text style="text-align: justify;">-->
+<!--                        The Evaluator allows the user to compare between a ground truth set (tagged set of images)-->
+<!--                        and the output of a classification algorithm. The Evaluator creates a confusion matrix-->
+<!--                        comparing between the algorithm and the ground truth as well as a convenient display of-->
+<!--                        the images along with the result of the comparison for each instance.-->
+<!--                      </v-card-text>-->
+<!--                    </v-card>-->
+<!--                  </v-flex>-->
+<!--                </v-layout>-->
+<!--              </v-container>-->
+<!--            </v-flex>-->
           </v-layout>
 
           <v-layout row wrap justify-center class="my-5">
@@ -94,7 +94,7 @@
                       <v-list-tile-title style="padding-left: 20%">
                         <div style="display: inline">
                           <img src="https://image.flaticon.com/icons/svg/25/25231.svg"style="width:15px;height:15px;">
-                          <a href="https://github.com/Idancw/IndustrialProject" target="_blank" class="align-middle">
+                          <a href="https://github.com/Idancw/IGETS_WebApp" target="_blank" class="align-middle">
                             GitHub
                           </a>
                         </div>
